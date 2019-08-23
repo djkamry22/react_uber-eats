4e1068584f4f95b9010e32324d399e20005ce302
Kamran <djkamry@gmail.com>
2019-08-23 10:42:34 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_uber-eats/precache-manifest.4831411afdd49c016f287f8976f8d4d0.js"
+  "/react_uber-eats/precache-manifest.06891129679d28c4e39db599dc79a102.js"
 );
 
 self.addEventListener('message', (event) => {
